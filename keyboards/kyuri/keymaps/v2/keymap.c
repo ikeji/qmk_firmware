93c559042852f68a8fd3d941a7dc8c3dea01bc23
Shun Ikejima <syun@ikejima.org>
2018-09-25 10:35:27 +0900

    Minor keymap update.

--- keyboards/kyuri/keymaps/v2/keymap.c
+++ keyboards/kyuri/keymaps/v2/keymap.c
@@ -39,6 +39,7 @@ enum custom_keycodes {
 #define GUI_N   GUI_T(KC_N)
 #define SFT_SPC SFT_T(KC_SPC)
 #define UP_B    LT(_UP, KC_B)
+#define UP_M    LT(_UP, KC_M)
 #define UP_N    LT(_UP, KC_N)
 #define UP_V    LT(_UP, KC_V)
 
@@ -49,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT(
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
         CTR_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-        SFT_SPC, FN_Z,    ALT_X,   GUI_C,   KC_V,    DOWN_B,  UP_N,    KC_M,    GUI_COM, ALT_DOT, CTR_SLA, FN_ENT
+        SFT_SPC, FN_Z,    ALT_X,   GUI_C,   DOWN_V,  DOWN_B,  UP_N,    UP_M,    GUI_COM, ALT_DOT, CTR_SLA, FN_ENT
   ),
   [_UP] = LAYOUT(
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
