28d16d74707a03709e9d2625ca3194d4a5cef2bf
Shun Ikejima <syun@ikejima.org>
2019-01-09 23:43:00 +0900

    Customize keymap.

--- keyboards/dmote/keymaps/default/keymap.c
+++ keyboards/dmote/keymaps/default/keymap.c
@@ -15,6 +15,12 @@
  */
 #include QMK_KEYBOARD_H
 
+#define _BASE 0
+#define _L 1
+#define _R 2
+#define _GUI 3
+#define _FN 6
+
 #define _______ KC_TRNS
 #define XXXXXXX KC_NO
 
@@ -23,12 +29,87 @@
 #define FN_FORD LALT(KC_RGHT)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-[0] = LAYOUT(
-  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
-  CANDE ,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT , \
-  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
-  CANDE ,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT \
+[_BASE] = LAYOUT(
+  KC_TAB ,KC_Q   ,KC_W   ,KC_E   ,KC_R   ,KC_T                     ,KC_Y   ,KC_U   ,KC_I   ,KC_O   ,KC_P   ,KC_BSPC, \
+  CANDE  ,KC_A   ,KC_S   ,KC_D   ,KC_F   ,KC_G                     ,KC_H   ,KC_J   ,KC_K   ,KC_L   ,KC_SCLN,KC_QUOT, \
+  KC_LSFT,KC_Z   ,KC_X   ,KC_C   ,KC_V   ,KC_B                     ,KC_N   ,KC_M   ,KC_COMM,KC_DOT ,KC_SLSH,KC_ENT , \
+                  XXXXXXX,XXXXXXX,XXXXXXX,                                  XXXXXXX,XXXXXXX,XXXXXXX,                 \
+                          XXXXXXX,                                                  XXXXXXX,                         \
+                                     KC_LCTL, KC_LALT,         KC_RALT,KC_RCTL,                                      \
+                                  MO(_L) ,KC_SPC ,KC_LGUI,  MO(_GUI),KC_SPC ,MO(_R) ,                                 \
+                          XXXXXXX,   MO(_FN), XXXXXXX,         XXXXXXX,MO(_FN),     XXXXXXX                          \
+
   ),
+[_L] = LAYOUT(
+  KC_TILD,KC_EXLM,KC_AT  ,KC_HASH,KC_DLR ,KC_PERC                  ,KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,_______, \
+  _______,_______,_______,_______,_______,_______                  ,_______,KC_UNDS,KC_PLUS,KC_LCBR,KC_RCBR,KC_PIPE, \
+  _______,_______,_______,_______,_______,_______                  ,_______,_______,_______,_______,_______,_______, \
+                  _______,_______,_______,                                  _______,_______,_______,                 \
+                          _______,                                                  _______,                         \
+                                     _______, _______,         _______,_______,                                      \
+                                  _______,_______,_______,  _______,_______,_______,                                 \
+                          _______,   _______, _______,         _______,_______,     _______                          \
+
+  ),
+[_R] = LAYOUT(
+  KC_GRV ,KC_1   ,KC_2   ,KC_3   ,KC_4   ,KC_5                     ,KC_6   ,KC_7   ,KC_8   ,KC_9   ,KC_0   ,_______, \
+  _______,_______,_______,_______,_______,_______                  ,_______,KC_MINS,KC_EQL ,KC_LBRC,KC_RBRC,KC_BSLS, \
+  _______,_______,_______,_______,_______,_______                  ,_______,_______,_______,_______,_______,_______, \
+                  _______,_______,_______,                                  _______,_______,_______,                 \
+                          _______,                                                  _______,                         \
+                                     _______, _______,         _______,_______,                                      \
+                                  _______,_______,_______,  _______,_______,_______,                                 \
+                          _______,   _______, _______,         _______,_______,     _______                          \
+
+  ),
+#define G(kc) LGUI(kc)
+
+[_GUI] = LAYOUT(
+  _______,G(KC_1),G(KC_2),G(KC_3),G(KC_4),G(KC_5)                  ,G(KC_6),G(KC_7),G(KC_8),G(KC_9),G(KC_0),_______, \
+  _______,G(KC_Q),G(KC_W),G(KC_E),G(KC_R),_______                  ,_______,_______,_______,_______,_______,_______, \
+  _______,G(KC_Z),G(KC_X),G(KC_C),G(KC_V),_______                  ,_______,_______,_______,_______,_______,_______, \
+                  _______,_______,_______,                                  _______,_______,_______,                 \
+                          _______,                                                  _______,                         \
+                                     _______, _______,         _______,_______,                                      \
+                                  _______,_______,_______,  _______,_______,_______,                                 \
+                          _______,   _______, _______,         _______,_______,     _______                          \
+
+  ),
+#undef G
+[_FN] = LAYOUT(
+  _______,KC_F1  ,KC_F2  ,KC_F3  ,KC_F4  ,KC_F5                    ,KC_F6  ,KC_F7  ,KC_F8  ,KC_F9  ,KC_F10 ,_______, \
+  _______,KC_F11 ,KC_F12 ,_______,_______,_______                  ,KC_LEFT,KC_DOWN,KC_UP  ,KC_RGHT,KC_PGUP,_______, \
+  _______,_______,_______,_______,_______,_______                  ,KC_HOME,KC_END ,FN_BACK,FN_FORD,KC_PGDN,_______, \
+                  _______,_______,_______,                                  _______,_______,_______,                 \
+                          _______,                                                  _______,                         \
+                                     _______, _______,         _______,_______,                                      \
+                                  _______,_______,_______,  _______,_______,_______,                                 \
+                          _______,   _______, _______,         _______,_______,     _______                          \
+
+  ),
+/*
+[_] = LAYOUT(
+  XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX                  ,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX, \
+  XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX                  ,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX, \
+  XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX                  ,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX, \
+                  XXXXXXX,XXXXXXX,XXXXXXX,                                  XXXXXXX,XXXXXXX,XXXXXXX,                 \
+                          XXXXXXX,                                                  XXXXXXX,                         \
+                                     XXXXXXX, XXXXXXX,         XXXXXXX,XXXXXXX,                                      \
+                                  XXXXXXX,XXXXXXX,XXXXXXX,  XXXXXXX,XXXXXXX,XXXXXXX,                                 \
+                          XXXXXXX,   XXXXXXX, XXXXXXX,         XXXXXXX,XXXXXXX,     XXXXXXX                          \
+
+  ),
+[_] = LAYOUT(
+  _______,_______,_______,_______,_______,_______                  ,_______,_______,_______,_______,_______,_______, \
+  _______,_______,_______,_______,_______,_______                  ,_______,_______,_______,_______,_______,_______, \
+  _______,_______,_______,_______,_______,_______                  ,_______,_______,_______,_______,_______,_______, \
+                  _______,_______,_______,                                  _______,_______,_______,                 \
+                          _______,                                                  _______,                         \
+                                     _______, _______,         _______,_______,                                      \
+                                  _______,_______,_______,  _______,_______,_______,                                 \
+                          _______,   _______, _______,         _______,_______,     _______                          \
+
+  ),
+*/
 };
 
