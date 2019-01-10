1b8983032fc64c3710389a644814fb64de099e5c
Shun Ikejima <syun@ikejima.org>
2019-01-10 22:18:50 +0900

    Fix serial comminucation.

--- keyboards/dmote/config.h
+++ keyboards/dmote/config.h
@@ -238,3 +238,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* Bootmagic Lite key configuration */
 // #define BOOTMAGIC_LITE_ROW 0
 // #define BOOTMAGIC_LITE_COLUMN 0
+
+#define SOFT_SERIAL_PIN D0
+
