a157ea823d073767b3c49fa1078da98ba8d98ef2
Shun Ikejima <syun@ikejima.org>
2019-04-16 14:52:55 +0900

    Modify dmote keymap.

--- keyboards/dmote/keymaps/default/keymap.c
+++ keyboards/dmote/keymaps/default/keymap.c
@@ -18,26 +18,30 @@
 #define _BASE 0
 #define _L 1
 #define _R 2
-#define _GUI 3
+#define _GU 3
 #define _FN 6
 
 #define _______ KC_TRNS
 #define XXXXXXX KC_NO
 
 #define CANDE   CTL_T(KC_ESC)
+#define AANDT   ALT_T(KC_TAB)
+#define SANDE   SFT_T(KC_ENT)
+#define SANDS   SFT_T(KC_SPC)
 #define FN_BACK LALT(KC_LEFT)
 #define FN_FORD LALT(KC_RGHT)
+
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_BASE] = LAYOUT(
   KC_TAB ,KC_Q   ,KC_W   ,KC_E   ,KC_R   ,KC_T                     ,KC_Y   ,KC_U   ,KC_I   ,KC_O   ,KC_P   ,KC_BSPC, \
   CANDE  ,KC_A   ,KC_S   ,KC_D   ,KC_F   ,KC_G                     ,KC_H   ,KC_J   ,KC_K   ,KC_L   ,KC_SCLN,KC_QUOT, \
   KC_LSFT,KC_Z   ,KC_X   ,KC_C   ,KC_V   ,KC_B                     ,KC_N   ,KC_M   ,KC_COMM,KC_DOT ,KC_SLSH,KC_ENT , \
-                  XXXXXXX,XXXXXXX,XXXXXXX,                                  XXXXXXX,XXXXXXX,XXXXXXX,                 \
+                  KC_ESC ,KC_TAB ,KC_ENT ,                                  KC_BSPC,XXXXXXX,XXXXXXX,                 \
                           XXXXXXX,                                                  XXXXXXX,                         \
-                                     KC_LCTL, KC_LALT,         KC_RALT,KC_RCTL,                                      \
-                                  MO(_L) ,KC_SPC ,KC_LGUI,  MO(_GUI),KC_SPC ,MO(_R) ,                                 \
-                          RESET  ,   MO(_FN), KC_ENT ,         KC_BSPC,MO(_FN),     XXXXXXX                          \
+                                     CANDE  , AANDT  ,         AANDT  ,CANDE  ,                                      \
+                                  KC_ENT ,SANDS  ,MO(_L) ,  MO(_R) ,SANDS  ,KC_BSPC,                                 \
+                          RESET  ,   MO(_FN), KC_LGUI,         MO(_GU),MO(_FN),     XXXXXXX                          \
   ),
 [_L] = LAYOUT(
   KC_TILD,KC_EXLM,KC_AT  ,KC_HASH,KC_DLR ,KC_PERC                  ,KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,_______, \
