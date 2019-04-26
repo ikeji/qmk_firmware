97a0d6ba94106a44ced3e97b46fd8591d9d88991
Shun Ikejima <syun@ikejima.org>
2019-04-26 09:05:42 +0900

    Add tab dance timing.

--- keyboards/lets_split/keymaps/my/keymap.c
+++ keyboards/lets_split/keymaps/my/keymap.c
@@ -32,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_BASE] = LAYOUT( \
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
   CANDE ,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT , \
+  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, _______, \
   MO(_FN), KC_LCTL, KC_LALT, KC_LGUI, EANDL,   SANDS,   SANDS,   EANDR,   MO(_GUI),KC_RGUI, KC_RSFT, MO(_FN) \
 ),
 
@@ -70,3 +70,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 };
+
+uint16_t get_tapping_term(uint16_t keycode) {
+  switch (keycode) {
+    case SANDS:
+      return 3000;
+    case EANDL:
+    case EANDR:
+      return 100;
+    case CANDE:
+      return 200;
+    default:
+      return 500;
+  }
+}
