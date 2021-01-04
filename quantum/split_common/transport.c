da799da950384da255bc7bb03f3b02db2fdd5548
Shun Ikejima <syun@ikejima.org>
2021-01-04 22:46:21 +0900

    Another tune for trackball.

--- quantum/split_common/transport.c
+++ quantum/split_common/transport.c
@@ -324,8 +324,8 @@ bool transport_master(matrix_row_t matrix[]) {
 #    ifdef R_TRACKBALL_ENABLE
     if (serial_s2m_buffer.trackball_x !=0||
         serial_s2m_buffer.trackball_y !=0){
-      mousemove_y += -serial_s2m_buffer.trackball_y*3;
-      mousemove_x += -serial_s2m_buffer.trackball_x*3;
+      mousemove_y += -serial_s2m_buffer.trackball_y;
+      mousemove_x += -serial_s2m_buffer.trackball_x;
       /*
       print("send report\n");
       //xprintf("%02X\n", serial_s2m_buffer.trackball_y);
