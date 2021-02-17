bd8385283e727c44f7cca68d6c33694196163e78
Shun Ikejima <syun@ikejima.org>
2021-02-18 03:40:39 +0900

    [ikejikeypad] Alternative keymap for keypad.

--- keyboards/ikejikeypad/keymaps/default/keymap.c
+++ keyboards/ikejikeypad/keymaps/default/keymap.c
@@ -18,23 +18,36 @@
 // Defines names for use in layer keycodes and the keymap
 enum layer_names {
     _BASE,
-    _FN
+    _TEN,
+    _TEN2,
 };
 
 // Defines the keycodes used by our macros in process_record_user
 enum custom_keycodes {
     QMKBEST = SAFE_RANGE,
-    QMKURL
+    QMKURL,
+    ZZ
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    /* Base */
     [_BASE] = LAYOUT(
         KC_C, KC_D, KC_E, KC_F,
         KC_8, KC_9, KC_A, KC_B,
         KC_4, KC_5, KC_6, KC_7,
         KC_0, KC_1, KC_2, KC_3
-    )
+    ),
+    [_TEN] = LAYOUT(
+        KC_P7, KC_P8, KC_P9,   KC_NLCK,
+        KC_P4, KC_P5, KC_P6,   KC_BSPC,
+        KC_P1, KC_P2, KC_P3,   KC_TAB,
+        KC_P0, ZZ,    KC_PDOT, KC_PENT
+    ),
+    [_TEN2] = LAYOUT(
+        KC_7, KC_8, KC_9,    KC_NO,
+        KC_4, KC_5, KC_6,    KC_BSPC,
+        KC_1, KC_2, KC_3,    KC_TAB,
+        KC_0, ZZ,   KC_DOT, KC_PENT
+    ),
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
