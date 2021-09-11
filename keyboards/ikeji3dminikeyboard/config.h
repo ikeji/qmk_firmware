6a6693458ef336c9146b5730983be14e7140362d
Shun Ikejima <syun@ikejima.org>
2021-09-12 03:07:23 +0900

    [@3mk] Copy config from sono1.

--- keyboards/ikeji3dminikeyboard/config.h
+++ keyboards/ikeji3dminikeyboard/config.h
@@ -40,8 +40,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *                  ROW2COL = ROW = Anode (+), COL = Cathode (-, marked on diode)
  *
  */
-#define MATRIX_ROW_PINS { D0, D5 }
-#define MATRIX_COL_PINS { F1, F0, B0 }
+#define MATRIX_ROW_PINS { A6, A7 }
+#define MATRIX_COL_PINS { A0, A1, A2 }
 #define UNUSED_PINS
 
 /* COL2ROW, ROW2COL */
