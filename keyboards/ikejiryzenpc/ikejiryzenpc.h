6644acb67e980b5418781885796c0549eddfa395
Shun Ikejima <syun@ikejima.org>
2021-11-21 15:21:07 +0900

    [@ryzenpc] Add initial keymap.

--- keyboards/ikejiryzenpc/ikejiryzenpc.h
+++ keyboards/ikejiryzenpc/ikejiryzenpc.h
@@ -13,10 +13,22 @@
  * The second converts the arguments into a two-dimensional array which
  * represents the switch matrix.
  */
+
+#define XXX KC_Z
 #define LAYOUT( \
-    k00, k01, k02, \
-      k10,  k12    \
+    k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k0a, k0b, \
+    k10, k11, k12, k13, k14, k15, k16, k17, k18, k19, k1a, k1b, \
+    k20, k21, k22, k23, k24, k25, k26, k27, k28, k29, k2a, k2b, \
+    k30, k31, k32, k33, k34, k35, k36, k37, k38, k39, k3a, k3b \
 ) { \
-    { k00, k01,   k02 }, \
-    { k10, KC_NO, k12 }  \
+    { k00, k01, k02, k03, k04, k05, XXX, XXX, XXX, XXX }, \
+    { k10, k11, k12, k13, k14, k15, XXX, XXX, XXX, XXX }, \
+    { k20, k21, k22, k23, k24, k25, XXX, XXX, XXX, XXX }, \
+    { k30, k31, k32, k33, k34, k35, XXX, XXX, XXX, XXX }, \
+    { XXX, XXX, XXX, XXX, XXX, XXX, k06, k16, k26, k36 }, \
+    { XXX, XXX, XXX, XXX, XXX, XXX, k07, k17, k27, k37 }, \
+    { XXX, XXX, XXX, XXX, XXX, XXX, k08, k18, k28, k38 }, \
+    { XXX, XXX, XXX, XXX, XXX, XXX, k09, k19, k29, k39 }, \
+    { XXX, XXX, XXX, XXX, XXX, XXX, k0a, k1a, k2a, k3a }, \
+    { XXX, XXX, XXX, XXX, XXX, XXX, k0b, k1b, k2b, k3b }, \
 }
--- keyboards/ikejiryzenpc/keymaps/default/keymap.c
+++ keyboards/ikejiryzenpc/keymaps/default/keymap.c
@@ -3,20 +3,87 @@
 
 #include QMK_KEYBOARD_H
 
-// Defines names for use in layer keycodes and the keymap
-enum layer_names {
-    _BASE,
-    _FN
+#define _BASE 0
+#define _L 1
+#define _R 2
+#define _GUI 3
+#define _FN 6
+
+// Defines the keycodes used by our macros in process_record_user
+enum custom_keycodes {
+  MYKEY = SAFE_RANGE,
 };
 
+#define CANDE   CTL_T(KC_ESC)
+#define AANDT   ALT_T(KC_TAB)
+#define SANDE   SFT_T(KC_ENT)
+#define SANDS   SFT_T(KC_SPC)
+#define FN_BACK LALT(KC_LEFT)
+#define FN_FORD LALT(KC_RGHT)
+#define FN_BACK LALT(KC_LEFT)
+#define FN_FORD LALT(KC_RGHT)
+#define EANDL   LT(_L, KC_ENT)
+#define BANDL   LT(_L, KC_BSPC)
+#define EANDR   LT(_R, KC_ENT)
+#define EANDG   GUI_T(KC_ENT)
+#define EANDLG  LT(_GUI, KC_ENT)
+
+#define _______ KC_TRNS
+#define XXXXXXX KC_NO
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    /* Base */
-    [_BASE] = LAYOUT(
-        KC_A,    KC_1,    MO(_FN),
-            KC_TAB,   KC_SPC
-    ),
-    [_FN] = LAYOUT(
-        _______, _______,  _______,
-            RESET,    XXXXXXX
-    )
+[_BASE] = LAYOUT( \
+  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y, KC_U,  KC_I,    KC_O,    KC_P,    KC_BSPC, \
+  CANDE ,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H, KC_J,  KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N, KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_ENT, \
+  MO(_FN), KC_LALT, KC_LCTL, KC_LGUI, BANDL,   SANDS,   SANDS,EANDR,MO(_GUI), KC_RGUI, KC_RALT, MO(_FN) \
+),
+
+[_L] = LAYOUT( \
+  KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,   KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC, \
+  _______, _______, _______, _______, _______, _______, _______, KC_UNDS,   KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, \
+  _______, _______, _______, _______, _______, _______, _______, S(KC_NUHS),S(KC_NUBS),_______,_______,_______, \
+  _______, _______, _______, _______, _______, _______, _______, _______,   _______, _______, _______, _______ \
+),
+
+[_R] = LAYOUT( \
+  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
+  _______, _______, _______, _______, _______, _______, _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, \
+  _______, _______, _______, _______, _______, _______, _______, KC_NUHS, KC_NUBS, _______, _______, _______, \
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ \
+),
+
+#define G(kc) LGUI(kc)
+
+[_GUI] =  LAYOUT( \
+  _______, G(KC_1), G(KC_2), G(KC_3), G(KC_4), G(KC_5), G(KC_6), G(KC_7), G(KC_8), G(KC_9), G(KC_0), _______, \
+  _______, G(KC_Q), G(KC_W), G(KC_E), G(KC_R), _______, KC_INS,  _______, _______, _______, _______, _______, \
+  _______, G(KC_Z), G(KC_X), G(KC_C), G(KC_V), _______, _______, _______, _______, _______, _______, _______, \
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ \
+),
+
+#undef G
+
+
+[_FN] =  LAYOUT( \
+  RESET,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_DEL,  \
+  _______, KC_F11,  KC_F12,  _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_PGUP, KC_HOME, \
+  _______, _______, _______, _______, _______, _______, _______, _______, FN_BACK, FN_FORD, KC_PGDN, KC_END, \
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ \
+),
+
 };
+
+uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case SANDS:
+      return 3000;
+    case EANDL:
+    case EANDR:
+      return 100;
+    case CANDE:
+      return 200;
+    default:
+      return 500;
+  }
+}
