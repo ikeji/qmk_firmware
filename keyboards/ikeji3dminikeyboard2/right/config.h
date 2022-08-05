f2b3fb7af4be14aecc142285b2acccd502343576
Shun Ikejima <syun@ikejima.org>
2022-08-06 02:53:52 +0900

    [3mk2]Implement keyboard sides.

---
+++ keyboards/ikeji3dminikeyboard2/right/config.h
@@ -0,0 +1,28 @@
+// Copyright 2022 Shun Ikejima (@Shun Ikejima)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
+
+/* disable debug print */
+//#define NO_DEBUG
+
+/* disable print */
+//#define NO_PRINT
+
+/* disable action features */
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
+
+#define ANALOG_JOYSTICK_X_AXIS_PIN A1
+#define ANALOG_JOYSTICK_Y_AXIS_PIN A0
+
+#define SELECT_SOFT_SERIAL_SPEED 3
+#define DISABLE_SYNC_TIMER
+#define SPLIT_POINTING_ENABLE
+#define POINTING_DEVICE_COMBINED
