5c8b97c4427e550c34b868984c7122d24d16c25f
Shun Ikejima <syun@ikejima.org>
2022-10-28 21:53:19 +0900

    [fold] Initial keymap.

--- keyboards/ikejifold/keymaps/default/keymap.c
+++ keyboards/ikejifold/keymaps/default/keymap.c
@@ -12,10 +12,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * │ A │ B │ C │ D │ E │ F │ G │ H │ I │ J │ K │ L │
      * └───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┘
      */
-    [0] = LAYOUT_ortho_4x12(
+    [0] = LAYOUT_ortho_4x14(
         KC_A,    KC_B,    KC_C,    KC_D,    KC_E,    KC_F,    KC_G,    KC_H,    KC_I,    KC_J,    KC_K,    KC_L,
         KC_A,    KC_B,    KC_C,    KC_D,    KC_E,    KC_F,    KC_G,    KC_H,    KC_I,    KC_J,    KC_K,    KC_L,
         KC_A,    KC_B,    KC_C,    KC_D,    KC_E,    KC_F,    KC_G,    KC_H,    KC_I,    KC_J,    KC_K,    KC_L,
         KC_A,    KC_B,    KC_C,    KC_D,    KC_E,    KC_F,    KC_G,    KC_H,    KC_I,    KC_J,    KC_K,    KC_L
     )
 };
+
+void keyboard_post_init_user(void) {
+  // Customise these values to desired behaviour
+  debug_enable=true;
+  debug_matrix=true;
+  //debug_keyboard=true;
+  //debug_mouse=true;
+}
