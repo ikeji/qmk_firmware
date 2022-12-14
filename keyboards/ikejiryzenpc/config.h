d584936e4d2ae7f8ba4601f8bb915435aae0105d
Shun Ikejima <syun@ikejima.org>
2022-12-14 23:05:34 +0900

    [all] Fix build after sync to 0.19.2

--- keyboards/ikejiryzenpc/config.h
+++ keyboards/ikejiryzenpc/config.h
@@ -9,8 +9,8 @@
 #define VENDOR_ID    0xFEED
 #define PRODUCT_ID   0x0000
 #define DEVICE_VER   0x0001
-#define MANUFACTURER Shun Ikejima
-#define PRODUCT      ikejiryzenpc
+#define MANUFACTURER "Shun Ikejima"
+#define PRODUCT      "ikejiryzenpc"
 
 /* key matrix size */
 #define MATRIX_ROWS 10
