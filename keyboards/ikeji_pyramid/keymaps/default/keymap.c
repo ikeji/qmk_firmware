94b4cc791ddbbc4f14cee1a3761e2c84c7e3a4ab
Shun Ikejima <syun@ikejima.org>
2024-01-11 22:22:41 +0900

    [@pyramid] format code.

--- keyboards/ikeji_pyramid/keymaps/default/keymap.c
+++ keyboards/ikeji_pyramid/keymaps/default/keymap.c
@@ -49,7 +49,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
   CANDE ,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT, \
-  MO(_FN), KC_LALT, KC_LCTL, BANDL,   SANDS,KC_LGUI,MO(_GUI),   SANDS,      EANDR, KC_RGUI, KC_RALT, MO(_FN) \
+  BANDL    , SANDS   , KC_LGUI   ,
+  MO(_FN)  , KC_LALT , KC_LCTL   ,
+  KC_RGUI  , KC_RALT , MO(_FN) ,
+  MO(_GUI) , SANDS   , EANDR    \
 ),
 
 [_L] = LAYOUT( \
